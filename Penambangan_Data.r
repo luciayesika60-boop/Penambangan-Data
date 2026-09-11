09f39f796763208eccd62f0a3f5675e8c0516549
luciayesika60-boop <luciayesika60@gmail.com>
2026-09-11 19:18:50 +0700

    menambah karakteristik Data

--- Penambangan_Data.r
+++ Penambangan_Data.r
@@ -1,3 +1,9 @@
 #Membaca dataset
 data <- read.csv("data_penjualan (1).csv")
 print(data)
+#Melihat jumlah baris dan kolom
+dim(data)
+#Melihat struktur dan tipe data
+str(data)
+#Melihat ringkasan statistik dari dataset
+summary(data)
