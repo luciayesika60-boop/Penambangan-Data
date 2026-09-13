c0e79a0a9ca221069aeb163b283648a67afc1f65
rcrdoo10 <rcrdoo10@gmail.com>
2026-09-13 23:17:08 +0700

    Udah ada Standard Deviasi

--- Penambangan_Data.r
+++ Penambangan_Data.r
@@ -1,13 +1,24 @@
 #Membaca dataset
 data <- read.csv("penjualan_tiket_pesawat (1).csv")
 print(data)
+
 #Melihat jumlah baris dan kolom
 dim(data)
+
 #Melihat struktur dan tipe data
 str(data)
+
 #Melihat ringkasan statistik dari dataset
 summary(data)
+
 #Mengecek missing value
 colSums(is.na(data))
+
 #Mengecek duplikat data
 sum(duplicated(data))
+
+#Standard Deviasi Total, Ticket_Price, Ticket_Quantity, Transaction_ID
+sd(data$Total)           
+sd(data$Ticket_Price)    
+sd(data$Ticket_Quantity) 
+sd(data$Transaction_ID)  
